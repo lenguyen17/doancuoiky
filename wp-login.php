5bd1a1297ad51a403af608b0797908e0fe991e50
lenguyen17 <lenguyencm97@gmail.com>
2023-06-26 18:17:11 +0700

    SEO

--- wp-login.php
+++ wp-login.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en-US" class="colibri-wp-theme">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo;  &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="stylesheet" id="dashicons-css" href="https://lenguyen17.github.io/doancuoiky/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://lenguyen17.github.io/doancuoiky/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/forms.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/login.min.css?ver=6.2.2" media="all">
+<script type="text/javascript" data-cfasync="false" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
+<script type="text/javascript" data-cfasync="false" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<link rel="stylesheet" id="dashicons-css" href="https://lenguyen17.github.io/doancuoiky/wp-includes/css/dashicons.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://lenguyen17.github.io/doancuoiky/wp-includes/css/buttons.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/forms.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/l10n.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://lenguyen17.github.io/doancuoiky/wp-admin/css/login.min.css?ver=6.2.2" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -35,7 +37,7 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="http://localhost/doancuoiky/woocommerce/wp-admin/?simply_static_page=268">
+									<input type="hidden" name="redirect_to" value="http://localhost/doancuoiky/woocommerce/wp-admin/?simply_static_page=19505">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
@@ -48,25 +50,39 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
 			<a href="https://lenguyen17.github.io/doancuoiky/">&larr; Go to </a>		</p>
 			</div>
-			<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/lenguyen17.github.io\/doancuoiky\/wp-includes\/js\/zxcvbn.min.js"};</script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
-<script id="wp-i18n-js-after">
+			<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/ui/core.min.js?ver=1.13.2" id="jquery-ui-core-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/ui/menu.min.js?ver=1.13.2" id="jquery-ui-menu-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/dom-ready.min.js?ver=392bdd43726760d1f3ca" id="wp-dom-ready-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
-<script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/doancuoiky\/woocommerce\/wp-admin\/admin-ajax.php"}};</script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"6acb9766d6"};</script>
-<script src="https://lenguyen17.github.io/doancuoiky/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/dist/a11y.min.js?ver=ecce20f002eda4c19664" id="wp-a11y-js"></script>
+<script type="text/javascript" id="jquery-ui-autocomplete-js-extra">/*  */
+var uiAutocompleteL10n = {"noResults":"No results found.","oneResult":"1 result found. Use up and down arrow keys to navigate.","manyResults":"%d results found. Use up and down arrow keys to navigate.","itemSelected":"Item selected."};
+/*  */</script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/jquery/ui/autocomplete.min.js?ver=1.13.2" id="jquery-ui-autocomplete-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">/*  */
+var _zxcvbnSettings = {"src":"https:\/\/lenguyen17.github.io\/doancuoiky\/wp-includes\/js\/zxcvbn.min.js"};
+/*  */</script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" id="password-strength-meter-js-extra">/*  */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/*  */</script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">/*  */
+var _wpUtilSettings = {"ajax":{"url":"\/doancuoiky\/woocommerce\/wp-admin\/admin-ajax.php"}};
+/*  */</script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">/*  */
+var userProfileL10n = {"user_id":"0","nonce":"c348404b1a"};
+/*  */</script>
+<script type="text/javascript" src="https://lenguyen17.github.io/doancuoiky/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
